6f52c9e23ac16fbf7a2b6d85a4cebb65edc429b1
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-21 01:38:36 +0900

    apt update & upgrade

--- recipe.rb
+++ recipe.rb
@@ -2,3 +2,9 @@ execute "create mastodon user" do
     user "root"
     command "adduser mastodon"
 end
+
+execute "apt update & upgrade" do
+    user "root"
+    command "apt update && apt upgrade"
+end
+
