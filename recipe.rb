d23e0e7d1ac16b481c656e1744160114b8c01c50
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-21 02:12:15 +0900

    fix about

--- recipe.rb
+++ recipe.rb
@@ -7,7 +7,7 @@ package "curl" do
     action :install
 end
 
-execute "download node pkg & add" do
+execute "Add node.js Repository" do
     user "root"
     command "curl -sL https://deb.nodesource.com/setup_8.x | bash -"
 end
